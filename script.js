77a8dc94a606041f312e07c02944179e685495a0
Sajjad Zaidi <62520438+infology00@users.noreply.github.com>
2026-08-05 11:11:11 -1200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const megaParent = document.querySelector(".mega-parent");
+const megaMenu = document.querySelector(".mega-menu");
 
+let closeTimer;
+
+megaParent.addEventListener("mouseenter", () => {
+
+    clearTimeout(closeTimer);
+
+    megaMenu.classList.add("active");
+
+});
+
+megaParent.addEventListener("mouseleave", () => {
+
+    closeTimer = setTimeout(() => {
+
+        megaMenu.classList.remove("active");
+
+    },150);
+
+});
